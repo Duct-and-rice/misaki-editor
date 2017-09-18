19c19f82117a0cb504a39e44f4975c42cc8aa2e3
duct-and-rice <duct@duct>
2017-09-19 01:31:54 +0900

    singleton check

--- test/unit/specs/Lib.spec.js
+++ test/unit/specs/Lib.spec.js
@@ -1,9 +1,11 @@
+/* eslint no-duplicate-imports:0 */
 import DOMRuler from '@/lib/ruler'
+import DOMRuler2 from '@/lib/ruler'
 
 describe('DOMRuler', () => {
     it('DOMRuler must be a singleton', () => {
         let a = new DOMRuler()
-        let b = new DOMRuler()
+        let b = new DOMRuler2()
         expect(a).to.equal(b)
     })
 })
