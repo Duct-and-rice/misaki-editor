76bca14d29fda15d2b7e9cef9b24caec2a56c7bd
duct-and-rice <duct@duct>
2017-09-19 20:26:17 +0900

    add tabgroup unit test

--- test/unit/specs/TabGroup.spec.js
+++ test/unit/specs/TabGroup.spec.js
@@ -1,19 +1,15 @@
 import Vue from 'vue'
-import Main from '@/components/Main'
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
+import TabGroup from '@/components/TabGroup'
 import VuePhotonkit from 'vue-photonkit'
 
-describe('Main.vue', function () {
+describe('TabGroup.vue', function () {
     before(function () {
         Vue.use(VuePhotonkit)
-        Vue.component('app-header', Header)
-        Vue.component('app-footer', Footer)
     })
-    it('should render correct contents at the header', function () {
-        const Constructor = Vue.extend(Main)
-        const vm = new Constructor().$mount()
-        expect(vm.$el.querySelector('.main header .title').textContent)
-            .to.equal('Misaki Editor')
+    it('data is a function', function () {
+        expect(TabGroup.data).to.be.a('function')
+    })
+    it('tabs is a array', function () {
+        expect(TabGroup.data().tabs).to.be.a('Array')
     })
 })
