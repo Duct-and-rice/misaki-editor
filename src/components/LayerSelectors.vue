4233e596c855c8bd08e8eef3e70a258f796a839a
duct-and-rice <duct@duct>
2017-09-19 22:53:00 +0900

    create LayerSelectors

---
+++ src/components/LayerSelectors.vue
@@ -0,0 +1,66 @@
+<template lang="pug">
+.tab-group
+    ph-tab-item(v-for="layer in layers" :key="layer.title" :active='layer.active') {{ layer.title }}
+    .tab-item.tab-item-fixed
+        ph-icon(icon="plus")
+</template>
+
+<script>
+export default {
+    name: 'tab-group',
+    data () {
+        return {
+            layers: [
+                {title: 'a', active: true},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'},
+                {title: 'a'}
+            ]
+        }
+    }
+}
+</script>
+
+<style scoped>
+.tab-group {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    margin: 5px 10px;
+    width: calc(100% - 20px - 3px);
+    & .tab-item{
+        &:not(.tab-item-fixed) {
+            min-width: 100px;
+        }
+        &:first-child {
+            border-left: 1px solid #989698;
+        }
+        &:last-child {
+            border-right: 1px solid #989698;
+        }
+    }
+    border-bottom: none;
+
+    overflow-x: scroll;
+    overflow-y: hidden;
+}
+
+</style>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,11 +3,17 @@ ph-window.main
     app-header
     app-tab-group
     ph-window-content
+        ph-window
+            ph-pane-group
+                ph-pane
+                ph-pane(:sidebar="true" size="sm")
+            app-layer-selectors
     app-footer
 </template>
 
 <script>
 import TabGroup from './TabGroup'
+import LayerSelectors from './LayerSelectors'
 
 export default {
     name: 'main',
@@ -16,11 +22,11 @@ export default {
         }
     },
     components: {
-        'app-tab-group': TabGroup
+        'app-tab-group': TabGroup,
+        'app-layer-selectors': LayerSelectors
     }
 }
 </script>
 
 <style scoped>
-
 </style>
