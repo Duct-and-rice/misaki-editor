7b339f0c98f7f355629078ea5104fa88b7901a37
duct-and-rice <duct@duct>
2017-09-19 17:37:25 +0900

    mocha arrow -> function

--- test/unit/specs/Lib.spec.js
+++ test/unit/specs/Lib.spec.js
@@ -2,22 +2,30 @@
 import {DOMRuler, CanvasRuler} from '@/lib/ruler'
 import WebFont from 'webfontloader'
 
-describe('Ruler', () => {
-    describe('Singleton', () => {
-        it('DOMRuler must be a singleton', () => {
+function rulerSpeed (ruler, num, label) {
+    console.time(label)
+    for (let i = 0; i < num; i++) {
+        ruler.getWidth(i + '')
+    }
+    console.timeEnd(label)
+}
+
+describe('Ruler', function () {
+    describe('Singleton', function () {
+        it('DOMRuler must be a singleton', function () {
             const a = new DOMRuler()
             const b = new DOMRuler()
             expect(a).to.equal(b)
         })
 
-        it('CanvasRuler must be a singleton', () => {
+        it('CanvasRuler must be a singleton', function () {
             const a = new CanvasRuler()
             const b = new CanvasRuler()
             expect(a).to.equal(b)
         })
     })
 
-    describe('Width', () => {
+    describe('Width', function () {
         before(done => {
             WebFont.load({
                 custom: {
@@ -28,17 +36,25 @@ describe('Ruler', () => {
                 }
             })
         })
-        it('DOMRuler Spaces', done => {
+        it('DOMRuler Spaces', function () {
             const ruler = new DOMRuler()
             expect(ruler.getWidth(' ')).to.equal(5)
             expect(ruler.getWidth('　')).to.equal(11)
-            done()
         })
-        it('CanvasRuler Spaces', done => {
+        it('CanvasRuler Spaces', function () {
             const ruler = new CanvasRuler()
             expect(ruler.getWidth(' ')).to.equal(5)
             expect(ruler.getWidth('　')).to.equal(11)
-            done()
+        })
+        it('Measure DOMRuler Speed', function () {
+            this.timeout(10000)
+            const ruler = new DOMRuler()
+            rulerSpeed(ruler, 10000, 'DOMRuler')
+        })
+        it('Measure CanvasRuler Speed', function () {
+            this.timeout(10000)
+            const ruler = new CanvasRuler()
+            rulerSpeed(ruler, 10000, 'CanvasRuler')
         })
     })
 })
--- test/unit/specs/Main.spec.js
+++ test/unit/specs/Main.spec.js
@@ -4,13 +4,13 @@ import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import VuePhotonkit from 'vue-photonkit'
 
-describe('Main.vue', () => {
-    before(() => {
+describe('Main.vue', function () {
+    before(function () {
         Vue.use(VuePhotonkit)
         Vue.component('app-header', Header)
         Vue.component('app-footer', Footer)
     })
-    it('should render correct contents at the header', () => {
+    it('should render correct contents at the header', function () {
         const Constructor = Vue.extend(Main)
         const vm = new Constructor().$mount()
         expect(vm.$el.querySelector('.main header .title').textContent)
