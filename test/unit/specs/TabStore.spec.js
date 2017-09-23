df19ec6df7b1aacd47f31245e91152a3e57a178f
duct-and-rice <duct@duct>
2017-09-24 00:32:28 +0900

    add unit tests

--- test/unit/specs/TabStore.spec.js
+++ test/unit/specs/TabStore.spec.js
@@ -4,20 +4,51 @@ import types from '../../../src/store/tab/mutation-types'
 
 describe('Store', function () {
     describe('Mutations', function () {
-        it('addTab', function () {
+        it('add', function () {
             const store = {tabs: []}
-            mutations.addTab(store, {title: 'test'})
+            mutations.add(store, {title: 'test'})
             expect(store.tabs[0]).to.have.property('title', 'test')
+        })
+
+        it('select', function () {
+            const store = {tabs: []}
+            mutations.select(store, 0)
+            expect(store.current).to.equal(0)
+        })
+
+        it('select', function () {
+            const store = {tabs: []}
+            mutations.add(store, {title: 'test'})
+            mutations.select(store, 0)
+            expect(store.tabs.length).to.equal(1)
         })
     })
     describe('Actions', function () {
-        it('addNewTab', function (done) {
+        it('add', function (done) {
             const commit = (type, newTab) => {
-                expect(type).to.equal(types.ADD_TAB)
+                expect(type).to.equal(types.ADD)
                 expect(newTab).to.have.property('title', 'New Tab')
                 done()
             }
-            actions.addNewTab({commit})
+            actions.add({commit})
+        })
+
+        it('select', function (done) {
+            const commit = (type, index) => {
+                expect(type).to.equal(types.SELECT)
+                expect(index).to.equal(0)
+                done()
+            }
+            actions.select({commit}, 0)
+        })
+
+        it('close', function (done) {
+            const commit = (type, index) => {
+                expect(type).to.satisfy(type => type === types.CLOSE || type === types.SELECT)
+                expect(index).to.equal(0)
+                done()
+            }
+            actions.close({commit}, 0)
         })
     })
 })
