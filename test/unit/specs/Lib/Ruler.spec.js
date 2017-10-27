69b80a355974616738d53cd4e86fcd17be2c7185
duct-and-rice <duct@duct>
2017-10-27 21:49:35 +0900

    adjust specs

--- test/unit/specs/Lib/Ruler.spec.js
+++ test/unit/specs/Lib/Ruler.spec.js
@@ -1,5 +1,5 @@
 import {DOMRuler, CanvasRuler} from '@/lib/ruler'
-import {SPACES, generateSpaceFromAH, adjustWithUnicode, default as widthSpace} from '@/lib/space'
+import {SPACES} from '@/lib/space'
 import WebFont from 'webfontloader'
 
 describe('Ruler', function () {
@@ -69,39 +69,6 @@ describe('Ruler', function () {
             ruler.isLock = true
             expect(() => ruler.getWidth('')).to.throw()
             ruler.isLock = false
-        })
-    })
-})
-
-describe('widthSpace', function () {
-    it('Ideographic Space', function () {
-        expect(widthSpace(11)).to.equal('　')
-    })
-    it('Spaces', function () {
-        const ruler = new CanvasRuler()
-        for (let i = 1; i < 1000; i++) {
-            const sp = widthSpace(i)
-            expect(ruler.getWidth(sp)).to.equal(i)
-            expect(sp.charAt(0)).not.to.equal(' ')
-            expect(sp).not.to.include('  ')
-        }
-    })
-    it('Adjust with Unicode', function () {
-        const ruler = new CanvasRuler()
-        expect(ruler.getWidth(adjustWithUnicode(1))).to.equal(1)
-    })
-    it('generateSpaceFromAH returns half space', function () {
-        expect(generateSpaceFromAH(0, 1)).to.equal(' ')
-    })
-    describe('Error Check', function () {
-        it('generateSpaceFromAH', function () {
-            expect(() => generateSpaceFromAH(-1, -1)).to.throw()
-        })
-        it('adjustWithUnicode', function () {
-            expect(() => adjustWithUnicode(50)).to.throw()
-        })
-        it('widthSpace', function () {
-            expect(() => widthSpace('a')).to.throw()
         })
     })
 })
---
+++ test/unit/specs/Lib/Space.spec.js
@@ -0,0 +1,34 @@
+import {CanvasRuler} from '@/lib/ruler'
+import {generateSpaceFromAH, adjustWithUnicode, default as widthSpace} from '@/lib/space'
+describe('Space', function () {
+    it('Ideographic Space', function () {
+        expect(widthSpace(11)).to.equal('　')
+    })
+    it('Spaces', function () {
+        const ruler = new CanvasRuler()
+        for (let i = 1; i < 1000; i++) {
+            const sp = widthSpace(i)
+            expect(ruler.getWidth(sp)).to.equal(i)
+            expect(sp.charAt(0)).not.to.equal(' ')
+            expect(sp).not.to.include('  ')
+        }
+    })
+    it('Adjust with Unicode', function () {
+        const ruler = new CanvasRuler()
+        expect(ruler.getWidth(adjustWithUnicode(1))).to.equal(1)
+    })
+    it('generateSpaceFromAH returns half space', function () {
+        expect(generateSpaceFromAH(0, 1)).to.equal(' ')
+    })
+    describe('Error Check', function () {
+        it('generateSpaceFromAH', function () {
+            expect(() => generateSpaceFromAH(-1, -1)).to.throw()
+        })
+        it('adjustWithUnicode', function () {
+            expect(() => adjustWithUnicode(50)).to.throw()
+        })
+        it('widthSpace', function () {
+            expect(() => widthSpace('a')).to.throw()
+        })
+    })
+})
